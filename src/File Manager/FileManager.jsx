45864f28ad5b7871d455e6287e88f874ed13c85a
Saifullah-dev <saifullah.contact@gmail.com>
2024-08-11 14:07:52 +0500

    File/Folder Paste: Duplicate Name Handled

--- src/File Manager/FileManager.jsx
+++ src/File Manager/FileManager.jsx
@@ -8,15 +8,7 @@ import Modal from "../components/Modal/Modal";
 import Button from "../components/Button/Button";
 import { IoWarningOutline } from "react-icons/io5";
 
-const allowedFileExtensions = [
-  ".txt",
-  ".png",
-  ".jpg",
-  ".jpeg",
-  ".pdf",
-  ".doc",
-  ".docx",
-];
+const allowedFileExtensions = [".txt", ".png", ".jpg", ".jpeg", ".pdf", ".doc", ".docx"];
 
 const FileManager = () => {
   // States
@@ -97,35 +89,59 @@ const FileManager = () => {
     delete file.children;
     return [
       { ...file, path: pastePath },
-      ...children.flatMap((child) =>
-        getCopiedFiles(child, pastePath + "/" + file.name)
-      ),
+      ...children.flatMap((child) => getCopiedFiles(child, pastePath + "/" + file.name)),
     ];
   };
 
-  const handlePaste = (e, pastePath, filesCopied) => {
-    if (clipBoard.isMoving) {
-      setFiles((prev) => {
-        const filteredFiles = prev.filter((f) => {
-          return !filesCopied.find(
-            (cf) => cf.name === f.name && cf.path === f.path
-          );
-        });
+  const handleDuplicateFile = (file, pastePath, pastePathFiles) => {
+    if (file.path === pastePath || pastePathFiles.find((f) => f.name === file.name)) {
+      const fileExtension = file.isDirectory ? "" : "." + file.name.split(".").pop();
+      const fileName = file.isDirectory ? file.name : file.name.split(".").slice(0, -1).join(".");
 
-        return [
-          ...filteredFiles,
-          ...clipBoard.files.flatMap((file) => getCopiedFiles(file, pastePath)),
-        ];
+      // Generating new file name for duplicate file
+      let maxFileNum = 0;
+      // If there exists a file with name fileName (1), fileName (2), etc.
+      // Check if the number is greater than the maxFileNum, then set it to that greater number
+      const fileNameRegex = new RegExp(`${fileName} \\(\\d+\\)`);
+      pastePathFiles.forEach((f) => {
+        const fName = f.isDirectory ? f.name : f.name.split(".").slice(0, -1).join(".");
+        if (fileNameRegex.test(fName)) {
+          const fileNumStr = fName.split(`${fileName} (`).pop().slice(0, -1);
+          const fileNum = parseInt(fileNumStr);
+          if (!isNaN(fileNum) && fileNum > maxFileNum) {
+            maxFileNum = fileNum;
+          }
+        }
       });
-      setClipBoard(null);
+      const appendNum = ` (${++maxFileNum})`;
+      const newFileName = fileName + appendNum + fileExtension;
+      //
+
+      return { ...file, name: newFileName };
     } else {
-      setFiles((prev) => {
-        return [
-          ...prev,
-          ...clipBoard.files.flatMap((file) => getCopiedFiles(file, pastePath)),
-        ];
-      });
+      return file;
     }
+  };
+
+  const handlePaste = (e, pastePath, filesCopied) => {
+    setFiles((prevFiles) => {
+      if (clipBoard.isMoving) {
+        prevFiles = prev.filter((f) => {
+          return !filesCopied.find((cf) => cf.name === f.name && cf.path === f.path);
+        });
+      }
+
+      return [
+        ...prevFiles,
+        ...clipBoard.files.flatMap((file) => {
+          const pastePathFiles = prevFiles.filter((f) => f.path === pastePath);
+          const nonDuplicateFile = handleDuplicateFile(file, pastePath, pastePathFiles);
+          return getCopiedFiles(nonDuplicateFile, pastePath);
+        }),
+      ];
+    });
+
+    clipBoard.isMoving && setClipBoard(null);
     setIsItemSelection(false);
     setSelectedFile(null);
   };
@@ -142,9 +158,7 @@ const FileManager = () => {
       });
     } else {
       setFiles((prev) => {
-        return prev.filter(
-          (f) => !(f.name === file.name && f.path === file.path)
-        );
+        return prev.filter((f) => !(f.name === file.name && f.path === file.path));
       });
     }
     setShowDelete(false);
@@ -162,8 +176,7 @@ const FileManager = () => {
         renameFileRef?.current?.select();
       } else {
         const fileExtension = selectedFile.name.split(".").pop();
-        const fileNameLength =
-          selectedFile.name.length - fileExtension.length - 1;
+        const fileNameLength = selectedFile.name.length - fileExtension.length - 1;
         renameFileRef?.current?.setSelectionRange(0, fileNameLength);
       }
     } else {
@@ -174,18 +187,14 @@ const FileManager = () => {
   const handleFileRename = (e, isConfirmed = false) => {
     if (renameFile === "") {
       setFileRenameError(true);
-      setRenameErrorMessage(
-        `${selectedFile.isDirectory ? "Folder" : "File"} name is required!`
-      );
+      setRenameErrorMessage(`${selectedFile.isDirectory ? "Folder" : "File"} name is required!`);
       return;
     } else if (renameFile === selectedFile.name) {
       setShowRename(false);
       return;
     } else if (currentPathFiles.some((file) => file.name === renameFile)) {
       setFileRenameError(true);
-      setRenameErrorMessage(
-        "A file or folder with the same name already exists!"
-      );
+      setRenameErrorMessage("A file or folder with the same name already exists!");
       return;
     } else if (!selectedFile.isDirectory && !isConfirmed) {
       const fileExtension = selectedFile.name.split(".").pop();
@@ -198,22 +207,16 @@ const FileManager = () => {
 
     setFiles((prev) => {
       return prev.map((file) => {
-        if (
-          file.name === selectedFile?.name &&
-          file.path === selectedFile?.path
-        ) {
+        if (file.name === selectedFile?.name && file.path === selectedFile?.path) {
           return {
             // Rename the file itself
             ...file,
             name: renameFile,
           };
-        } else if (
-          file.path.startsWith(selectedFile.path + "/" + selectedFile.name)
-        ) {
-          // All files in the folder
+        } else if (file.path.startsWith(selectedFile.path + "/" + selectedFile.name)) {
+          // Path update for all files in the folder
           const basePath = selectedFile.path + "/" + selectedFile.name;
-          const newBasePath =
-            basePath.split("/").slice(0, -1).join("/") + "/" + renameFile;
+          const newBasePath = basePath.split("/").slice(0, -1).join("/") + "/" + renameFile;
           const newPath = newBasePath + file.path.slice(basePath.length);
           return {
             ...file,
@@ -263,8 +266,7 @@ const FileManager = () => {
     // Calculate new sizes based on mouse movement
     const container = containerRef.current;
     const containerRect = container.getBoundingClientRect();
-    const newCol1Size =
-      ((e.clientX - containerRect.left) / containerRect.width) * 100;
+    const newCol1Size = ((e.clientX - containerRect.left) / containerRect.width) * 100;
 
     if (newCol1Size >= 15 && newCol1Size <= 60) {
       setColSizes({ col1: newCol1Size, col2: 100 - newCol1Size });
@@ -315,11 +317,7 @@ const FileManager = () => {
         className="files-container"
       >
         <div className="navigation-pane" style={{ width: colSizes.col1 + "%" }}>
-          <NavigationPane
-            files={files}
-            currentPath={currentPath}
-            setCurrentPath={setCurrentPath}
-          />
+          <NavigationPane files={files} currentPath={currentPath} setCurrentPath={setCurrentPath} />
           <div
             className={`sidebar-resize ${isDragging ? "sidebar-dragging" : ""}`}
             onMouseDown={handleMouseDown}
@@ -327,10 +325,7 @@ const FileManager = () => {
         </div>
 
         <div className="folers-preview" style={{ width: colSizes.col2 + "%" }}>
-          <BreadCrumb
-            currentPath={currentPath}
-            setCurrentPath={setCurrentPath}
-          />
+          <BreadCrumb currentPath={currentPath} setCurrentPath={setCurrentPath} />
           <Files
             currentPathFiles={currentPathFiles}
             setCurrentPath={setCurrentPath}
@@ -350,12 +345,7 @@ const FileManager = () => {
       </section>
 
       {/* Delete Folder/File */}
-      <Modal
-        heading={"Delete"}
-        show={showDelete}
-        setShow={setShowDelete}
-        dialogClassName={"w-25"}
-      >
+      <Modal heading={"Delete"} show={showDelete} setShow={setShowDelete} dialogClassName={"w-25"}>
         <div className="file-delete-confirm">
           <p className="file-delete-confirm-text">
             Are you sure you want to delete {selectedFile?.name}?
@@ -373,28 +363,20 @@ const FileManager = () => {
       {/* Delete Folder/File */}
 
       {/* Rename Folder/File */}
-      <Modal
-        heading={"Rename"}
-        show={showRename}
-        setShow={setShowRename}
-        dialogClassName={"w-25"}
-      >
+      <Modal heading={"Rename"} show={showRename} setShow={setShowRename} dialogClassName={"w-25"}>
         {renameFileWarning ? (
           <div className="fm-rename-folder-container">
             <div className="fm-rename-folder-input">
               <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
                 <IoWarningOutline size={70} color="orange" />
                 <div>
-                  If you change a file name extension, the file might become
-                  unusable. Are you sure you want to change it?
+                  If you change a file name extension, the file might become unusable. Are you sure
+                  you want to change it?
                 </div>
               </div>
             </div>
             <div className="fm-rename-folder-action">
-              <Button
-                type="secondary"
-                onClick={() => setRenameFileWarning(false)}
-              >
+              <Button type="secondary" onClick={() => setRenameFileWarning(false)}>
                 No
               </Button>
               <Button type="danger" onClick={(e) => handleFileRename(e, true)}>
@@ -416,9 +398,7 @@ const FileManager = () => {
                 onKeyDown={handleValidateFolderRename}
                 className="action-input"
               />
-              {fileRenameError && (
-                <div className="folder-error">{renameErrorMessage}</div>
-              )}
+              {fileRenameError && <div className="folder-error">{renameErrorMessage}</div>}
             </div>
             <div className="fm-rename-folder-action">
               <Button onClick={handleFileRename} type="primary">
