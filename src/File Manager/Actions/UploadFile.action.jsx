f54ebddca73316132182a0a56fcd96cc00e10ab0
Saifullah-dev <saifullah.contact@gmail.com>
2024-08-17 15:36:23 +0500

    Progress Component + Choose File Functionality

--- src/File Manager/Actions/UploadFile.action.jsx
+++ src/File Manager/Actions/UploadFile.action.jsx
@@ -3,30 +3,56 @@ import Button from "../../components/Button/Button";
 import { AiOutlineClose, AiOutlineCloudUpload } from "react-icons/ai";
 import { getFileExtension } from "../../utils/getFileExtension";
 import { useFileIcons } from "../../hooks/useFileIcons";
+import { FaRegFile } from "react-icons/fa6";
+import Progress from "../../components/Progress/Progress";
 
 const UploadFileAction = ({ onFilesSelected }) => {
-  const fileIcons = useFileIcons(33);
   const [files, setFiles] = useState([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const fileIcons = useFileIcons(33);
 
   const handleDrop = (e) => {
     e.preventDefault();
+    setIsDragging(false);
     const droppedFiles = e.dataTransfer.files;
     if (droppedFiles.length > 0) {
       setFiles((prev) => [...prev, ...droppedFiles]);
     }
   };
 
+  const handleChooseFile = (e) => {
+    const selectedFiles = e.target.files;
+    if (selectedFiles.length > 0) {
+      setFiles((prev) => [...prev, ...selectedFiles]);
+    }
+  };
+
   return (
     <div className={`fm-upload-file ${files.length > 0 ? "file-selcted" : ""}`}>
-      <div className="select-files" onDrop={handleDrop} onDragOver={(e) => e.preventDefault()}>
-        <div className="draggable-file-input">
+      <div className="select-files">
+        <div
+          className={`draggable-file-input ${isDragging ? "dragging" : ""}`}
+          onDrop={handleDrop}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={() => setIsDragging(true)}
+          onDragLeave={() => setIsDragging(false)}
+        >
           <div className="input-text">
             <AiOutlineCloudUpload size={30} />
             <span>Drag files to upload</span>
           </div>
         </div>
         <div className="btn-choose-file">
-          <Button>Choose File</Button>
+          <Button padding="0">
+            <label htmlFor="chooseFile">Choose File</label>
+            <input
+              type="file"
+              id="chooseFile"
+              className="choose-file-input"
+              onChange={handleChooseFile}
+              multiple
+            />
+          </Button>
         </div>
       </div>
       {files.length > 0 && (
@@ -35,19 +61,22 @@ const UploadFileAction = ({ onFilesSelected }) => {
           <ul>
             {files.map((file, index) => (
               <li key={index}>
-                <div className="file-icon">{fileIcons[getFileExtension(file.name)]}</div>
+                <div className="file-icon">
+                  {fileIcons[getFileExtension(file.name)] ?? <FaRegFile size={33} />}
+                </div>
                 <div className="file">
                   <div className="file-details">
                     <div className="file-info">
-                      <span className="file-name text-truncate">{file.name}</span>
+                      <span className="file-name text-truncate" title={file.name}>
+                        {file.name}
+                      </span>
                       <span className="file-size"> {file.size / 1000} KB</span>
                     </div>
                     <div className="rm-file" title="remove">
                       <AiOutlineClose />
                     </div>
                   </div>
-                  <div className="upload-progress-bar"></div>
-                  <div className="upload-progress">58% done</div>
+                  <Progress percent={20} />
                 </div>
               </li>
             ))}
