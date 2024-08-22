11494425ce6cdef5e3ddccdbd804df7a39608683
Saifullah-dev <saifullah.contact@gmail.com>
2024-08-22 12:45:05 +0500

    feat: first release

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as FileManager } from "./File Manager/FileManager";
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "./src/index.js",
+      name: "ReactFileManager",
+      fileName: (format) => `react-file-manager.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
