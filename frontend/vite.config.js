fbb6e5c5cbe9a42030c73d0ecd944f4fd4c25cd4
Saifullah-dev <saifullah.contact@gmail.com>
2024-09-07 18:49:09 +0500

    fix(build): correct config for ESM release only

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       entry: "./src/index.js",
       name: "ReactFileManager",
       fileName: (format) => `react-file-manager.${format}.js`,
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
