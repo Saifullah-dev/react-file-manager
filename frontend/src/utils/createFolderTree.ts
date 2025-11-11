ccc7e215b17b3db42f352f5ca77a480b01244f36
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2025-11-11 19:16:37 +0100

    Updated createFolderTree.ts
    Added correct type annotations

--- frontend/src/utils/createFolderTree.ts
+++ frontend/src/utils/createFolderTree.ts
@@ -1,4 +1,10 @@
-export const createFolderTree = (copiedFile, files) => {
+import { File } from "../types/File";
+
+type FileTree = File & {
+  children: FileTree[];
+};
+
+export const createFolderTree = (copiedFile : File, files : File[]) : FileTree => {
   const childFiles = files.filter(
     (f) => f.path === copiedFile.path + "/" + copiedFile.name
   );
