a0253a4138a3967be1a999ee33c0704ab98ab0e9
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2025-11-11 19:16:08 +0100

    Removed PropTypes package
    Does not seem to be working with TypeScript, removed for now.

--- frontend/src/FileManager/FileManager.tsx
+++ frontend/src/FileManager/FileManager.tsx
@@ -11,8 +11,6 @@ import { ClipBoardProvider } from "../contexts/ClipboardContext";
 import { LayoutProvider } from "../contexts/LayoutContext";
 import { useTriggerAction } from "../hooks/useTriggerAction";
 import { useColumnResize } from "../hooks/useColumnResize";
-import PropTypes from "prop-types";
-import { dateStringValidator, urlValidator } from "../validators/propValidators";
 import { TranslationProvider } from "../contexts/TranslationProvider";
 import { useMemo, useState } from "react";
 import { defaultPermissions } from "../constants";
@@ -227,65 +225,5 @@ const FileManager = ({
 };
 
 FileManager.displayName = "FileManager";
-
-FileManager.propTypes = {
-  files: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      isDirectory: PropTypes.bool.isRequired,
-      path: PropTypes.string.isRequired,
-      updatedAt: dateStringValidator,
-      size: PropTypes.number,
-    })
-  ).isRequired,
-  fileUploadConfig: PropTypes.shape({
-    url: urlValidator,
-    headers: PropTypes.objectOf(PropTypes.string),
-    method: PropTypes.oneOf(["POST", "PUT"]),
-  }),
-  isLoading: PropTypes.bool,
-  onCreateFolder: PropTypes.func,
-  onFileUploading: PropTypes.func,
-  onFileUploaded: PropTypes.func,
-  onRename: PropTypes.func,
-  onDelete: PropTypes.func,
-  onCut: PropTypes.func,
-  onCopy: PropTypes.func,
-  onPaste: PropTypes.func,
-  onDownload: PropTypes.func,
-  onLayoutChange: PropTypes.func,
-  onRefresh: PropTypes.func,
-  onFileOpen: PropTypes.func,
-  onFolderChange: PropTypes.func,
-  onSelect: PropTypes.func,
-  onSelectionChange: PropTypes.func,
-  onError: PropTypes.func,
-  layout: PropTypes.oneOf(["grid", "list"]),
-  maxFileSize: PropTypes.number,
-  enableFilePreview: PropTypes.bool,
-  filePreviewPath: urlValidator,
-  acceptedFileTypes: PropTypes.string,
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  initialPath: PropTypes.string,
-  filePreviewComponent: PropTypes.func,
-  primaryColor: PropTypes.string,
-  fontFamily: PropTypes.string,
-  language: PropTypes.string,
-  permissions: PropTypes.shape({
-    create: PropTypes.bool,
-    upload: PropTypes.bool,
-    move: PropTypes.bool,
-    copy: PropTypes.bool,
-    rename: PropTypes.bool,
-    download: PropTypes.bool,
-    delete: PropTypes.bool,
-  }),
-  collapsibleNav: PropTypes.bool,
-  defaultNavExpanded: PropTypes.bool,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  formatDate: PropTypes.func,
-};
 
 export default FileManager;
