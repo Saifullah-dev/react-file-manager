8b71fab1d9647ab7f44c04db132cc4c90ff5da06
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2025-11-12 20:55:55 +0100

    Fixed getAllFilesAPI.ts
    Defined response type.

--- frontend/src/api/getAllFilesAPI.ts
+++ frontend/src/api/getAllFilesAPI.ts
@@ -2,11 +2,11 @@ import { AxiosResponse } from "axios";
 import { api } from "./api";
 import { File } from "../types/File";
 
-export const getAllFilesAPI = async () => {
+export const getAllFilesAPI = async () : Promise<AxiosResponse<File[]>> => {
   try {
     const response = await api.get<File[]>("");
     return response;
   } catch (error) {
-    return error as AxiosResponse;
+    return error as AxiosResponse<File[]>;
   }
 };
