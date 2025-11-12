3edcba817ec15a4867aa2602f50d01a242196051
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2025-11-12 20:53:37 +0100

    Updated File.ts type
    Added parameter `[key: string]: any` to `File` for JS-compatability, e.g. people setting an id in JS, not defined in the TypeScript type.
    Added `FileExtended` type for internal usage.

--- frontend/src/types/File.ts
+++ frontend/src/types/File.ts
@@ -4,4 +4,11 @@ export interface File {
   path: string;
   updatedAt?: string;
   size?: number;
+  [key: string]: any; // For JS-Compatability
+}
+
+export interface FileExtended extends File {
+  isEditing?: boolean;
+  key?: string | number;
+  subDirectories?: FileExtended[];
 }
